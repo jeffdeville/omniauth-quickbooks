95fde87b6eef6ba5df0b450bd4e1bd508346378a
Matthew B. Jones <matthebjones85@gmail.com>
2017-09-17 16:41:05 -0400

    (oauth2) QBO requires OAuth2 for all new apps

--- lib/omniauth/strategies/quickbooks.rb
+++ lib/omniauth/strategies/quickbooks.rb
@@ -1,21 +1,31 @@
-require "omniauth/strategies/oauth"
+require "omniauth/strategies/oauth2"
 
 module OmniAuth
   module Strategies
-    class Quickbooks < OmniAuth::Strategies::OAuth
-
-      args [:consumer_key, :consumer_secret]
-
-      option :client_options, {
-        :access_token_path  => "/oauth/v1/get_access_token",
-        :authorize_url      => "https://appcenter.intuit.com/Connect/Begin",
-        :request_token_path => "/oauth/v1/get_request_token",
-        :site               => "https://oauth.intuit.com",
-      }
+    class Quickbooks < OmniAuth::Strategies::OAuth2
+      option :name, "quickbooks"
+      option :authorize_options, [:scope, :state]
+      option :client_options,
+             site: "https://appcenter.intuit.com",
+             authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+             token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+      DEFAULT_SCOPE = "com.intuit.quickbooks.accounting"
 
       uid {
         request.params['realmId']
       }
+
+      def authorize_params
+        super.tap do |params|
+          %w[scope state].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+
+          params[:scope] ||= DEFAULT_SCOPE
+        end
+      end
 
     end
   end
